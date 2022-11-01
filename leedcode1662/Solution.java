0840871dda65543d58ea9762a01a0ba8cf764657
17 <2594338376@qq.com>
2022-11-01 08:25:47 +0800

    每日一题

---
+++ leedcode1662/Solution.java
@@ -0,0 +1,15 @@
+package leedcode.leedcode1662;
+
+public class Solution {
+    public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+        String str1 = "";
+        String str2 = "";
+        for (String s: word1) {
+            str1+=s;
+        }
+        for (String s: word2) {
+            str2+=s;
+        }
+        return str1.equals(str2);
+    }
+}

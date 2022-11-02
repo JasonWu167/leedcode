edc1a3de31f385dd0dd94ed43ce637c9c988d216
17 <2594338376@qq.com>
2022-11-02 22:38:05 +0800

    每日一题

--- TestDemo.java
+++ TestDemo.java
@@ -1,11 +1,18 @@
 package leedcode;
 
 
+import leedcode.leedcode1620.Solution;
+
 public class TestDemo {
 
 
     public static void main(String[] args) {
-
+        Solution solution = new Solution();
+        int[][] arr = {
+                {1,2,5},
+                {2,1,7},{3,1,9}
+        };
+        solution.bestCoordinate(arr,2);
         System.out.println(5^1);
     }
 }
